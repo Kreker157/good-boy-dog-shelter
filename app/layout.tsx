0a3d683d8c9df2b585616c9a7a23abbd685ead28
radoslav-stack <radoslavhudacok1331@gmail.com>
2026-02-13 23:12:04 +0100

    feat: use i18nProvider to load i18n

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist, Inter } from 'next/font/google';
+import I18nProvider from '@/i18n/I18nProvider';
 import './globals.css';
 import '@/i18n/config';
 
+const inter = Inter({ variable: '--font-inter', subsets: ['latin'] });
+
 const geistSans = Geist({
   variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
   subsets: ['latin'],
 });
 
@@ -25,8 +23,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body className={`${inter.variable}${geistSans.variable}`}>
+        <I18nProvider>{children}</I18nProvider>
       </body>
     </html>
   );
